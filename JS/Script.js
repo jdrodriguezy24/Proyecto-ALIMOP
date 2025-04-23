b9928b5e2f8d1881ae296665f33be9f030db88ae
jdrodriguezy24 <168936242+jdrodriguezy24@users.noreply.github.com>
2025-04-23 14:50:42 -0500

    Add files via upload

---
+++ JS/Script.js
@@ -0,0 +1,20 @@
+//Esta funcion es para la informacion en español
+function toggleInformacion(id) {
+  var informacion = document.getElementById(id);
+  if (informacion.style.display === "none") {
+    informacion.style.display = "block";
+  } else {
+    informacion.style.display = "none";
+  }
+}
+
+
+// Esta funcion es para la informacion en ingles
+function toggleInformation(id) {
+    var information = document.getElementById(id);
+    if (information.style.display === "none") {
+      information.style.display = "block";
+    } else {
+      information.style.display = "none";
+    }
+  }
